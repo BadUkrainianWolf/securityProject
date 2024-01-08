6a8fd69a4b047172cfcf5f78320399b0489614d5
kyrylo <kyrylo.tymchenko@nure.ua>
2024-01-08 08:50:59 -0500

    Fix executables (remove debug info logging)

--- Definitions.h
+++ Definitions.h
@@ -8,6 +8,6 @@ constexpr int FIRST_CLIENT_PORT = 3001;
 constexpr auto SERVER_DIRECTORY = "serverFiles";
 constexpr auto CLIENT_DIRECTORY = "clientFiles";
 
-constexpr bool IS_DEBUG = true;
+constexpr bool IS_DEBUG = false;
 
 #endif //SECURITYPROJECT_DEFINITIONS_H
